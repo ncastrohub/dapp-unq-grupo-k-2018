827ab18edb2e960bf8be68e263ee2c01b375697b
ncastrohub <nazarenomartincastro@gmail.com>
2018-03-20 18:48:57 -0300

    first test

---
+++ backend/carpnd/src/test/java/model/TestFirstClass.java
@@ -0,0 +1,15 @@
+package model;
+
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class TestFirstClass {
+
+    @Test
+    public void testTestCase() {
+        assertThat(1).isEqualTo(1);
+    }
+
+}
