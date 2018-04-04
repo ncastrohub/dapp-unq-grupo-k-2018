5c731d39edb6b5ea0401df710a3087826fc3e8c4
ncastrohub <nazarenomartincastro@gmail.com>
2018-04-04 16:58:48 -0300

    WIP crando modelo de fechas

--- backend/carpnd/src/main/java/model/DaySchedule.java
+++ backend/carpnd/src/main/java/model/DaySchedule.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DaySchedule {
 
@@ -22,6 +23,20 @@ public class DaySchedule {
 
     public List<Pair<LocalTime,LocalTime>> avaibleHours() {
         return this.avaibleHours;
+    }
+
+    public void reserveFromTo(Integer startHour, Integer endHour) {
+//        Que existe una hora de inicio de un segmento menor o igual al valor de inicio
+//        Que existe una hora de inicio de un segmento mayor o igual al valor de final
+
+        LocalTime startHourToTime = LocalTime.of(startHour, 00);
+        LocalTime endHourToTime = LocalTime.of(endHour, 00);
+        List<Pair<LocalTime, LocalTime>> containedTimeLapses = this.avaibleHours.stream().filter(t -> {
+            Boolean supportMinHour = t.getKey().isBefore(startHourToTime) || t.getKey().equals(startHourToTime);
+            Boolean supportMax = t.getValue().isAfter(endHourToTime) || t.getValue().equals(startHourToTime);
+            return supportMinHour && supportMax;
+        }).collect(Collectors.toList());
+
     }
 }
 
--- backend/carpnd/src/test/java/model/TestDaySchedule.java
+++ backend/carpnd/src/test/java/model/TestDaySchedule.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
 
 public class TestDaySchedule {
 
@@ -15,7 +16,7 @@ public class TestDaySchedule {
 
         DaySchedule day = new DaySchedule(LocalDate.now());
 
-        LocalTime startOfDay = LocalTime.MIN;
+        Integer startOfDay = HoursOf
         LocalTime midnigth = LocalTime.MAX;
         
         assertThat(day.avaibleHours().size()).isEqualTo(1);
@@ -23,5 +24,19 @@ public class TestDaySchedule {
     }
 
 
+    public void testWhenReserveATimeLapseOfDateThatTimeAreOccuped(){
+        DaySchedule day = new DaySchedule(LocalDate.now());
+        day.reserveFromTo(10, 15);
+
+        LocalTime expectedStartHour = LocalTime.of(00,00);
+        LocalTime expectedEndHour = LocalTime.of(10, 00);
+
+        LocalTime expectedStartHour2 = LocalTime.of(15,00);
+        LocalTime expectedEndHour2 = LocalTime.MAX;
+
+        assertThat(day.avaibleHours().size()).isEqualTo(2);
+        assertThat(day.avaibleHours().get(0)).isEqualTo(new Pair<>(expectedStartHour, expectedEndHour));
+
+    }
 
 }
