3d863f7315371f35aa8756c467fb10548545185b
Julia <juliatroilo@gmail.com>
2018-04-10 16:59:43 -0300

    Test estados

---
+++ backend/carpnd/src/test/java/model/TestConfirmedByBothSides.java
@@ -0,0 +1,24 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestConfirmedByBothSides {
+
+    @Test
+    void ownerAccepts() {
+    }
+
+    @Test
+    void ownerRejects() {
+    }
+
+    @Test
+    void userAccepts() {
+    }
+
+    @Test
+    void userRejects() {
+    }
+}
---
+++ backend/carpnd/src/test/java/model/TestInProcess.java
@@ -0,0 +1,24 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestInProcess {
+
+    @Test
+    void ownerAccepts() {
+    }
+
+    @Test
+    void ownerRejects() {
+    }
+
+    @Test
+    void userAccepts() {
+    }
+
+    @Test
+    void userRejects() {
+    }
+}
