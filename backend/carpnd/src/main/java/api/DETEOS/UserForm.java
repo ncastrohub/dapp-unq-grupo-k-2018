3f4d410b7513cf7794cd347c6894d8f90020187e
JuliaTroilo <juliatroilo@gmail.com>
2018-05-07 23:54:30 -0300

    UserForm.java

---
+++ backend/carpnd/src/main/java/api/DETEOS/UserForm.java
@@ -0,0 +1,26 @@
+package api.DETEOS;
+
+import me.geso.tinyvalidator.constraints.NotNull;
+import me.geso.tinyvalidator.constraints.Size;
+
+@Data
+public class UserForm {
+
+    public UserForm() {}
+
+    @Size(min=2, max=30)
+    @NotNull(message = "required")
+    public String name;
+
+    @Size(min=2, max=30)
+    @NotNull(message = "required")
+    public String lastName;
+
+    @Size(min=10, max=11)
+    @NotNull(message = "required")
+    public String cuil;
+
+    @Size(min=8 max=30)
+    @NotNull(message = "required")
+    public String email;
+}
