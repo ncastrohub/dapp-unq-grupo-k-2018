0895c411f52edc5b9f66af41f84ea9236c208450
JuliaTroilo <juliatroilo@gmail.com>
2018-06-09 05:14:29 -0300

    De https://auth0.com/blog/angular-2-authentication/

---
+++ frontend/carpnd/src/app/callback.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
+
+@Component({
+  selector: 'app-callback',
+  template: `
+    <p>
+      callback works!
+    </p>
+  `,
+  styles: []
+})
+export class CallbackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
