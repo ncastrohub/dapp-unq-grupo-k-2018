3a52ecc6a968790748c77ffbd8e64ea9734e2d3b
Nazareno Castro <nazarenomartincastro@gmail.com>
2018-06-28 01:06:02 -0300

    Casi que funciona el user edit

--- frontend/carpnd/src/app/user/useredit/useredit.component.ts
+++ frontend/carpnd/src/app/user/useredit/useredit.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
 import { UserServiceService } from '../service/user-service.service';
 import { Router } from '@angular/router';
-import { Subscription }   from 'rxjs';
 
 @Component({
   selector: 'app-useredit',
@@ -15,14 +14,13 @@ export class UsereditComponent implements OnInit {
   constructor(private service: UserServiceService, private router: Router) { }
   
   user:User;
-  subscription: Subscription;
 
   ngOnInit() {
-    this.subscription = this.service.userObservable.subscribe(user => this.user = user);
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.service.getUser().subscribe(
+      user => {
+        this.user = user
+      }
+    );
   }
 
   errorList = [];
