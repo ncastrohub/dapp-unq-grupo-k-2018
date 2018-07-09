d4084d8a929fdd69531883f8c355f3f322ae20bb
Nazareno Castro <nazarenomartincastro@gmail.com>
2018-07-09 19:50:15 -0300

    Traducido el home y el nav bar

--- frontend/carpnd/src/app/callback.component.ts
+++ frontend/carpnd/src/app/callback.component.ts
@@ -8,7 +8,7 @@ import { ActivatedRoute } from '@angular/router';
   template: `
    <div class="text-center">
         <i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>
-        <div>Working...</div>
+        <div>{{ "Working..." | translate }}</div>
     </div>
   `,
   styles: []
